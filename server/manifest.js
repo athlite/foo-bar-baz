acb52c17b42faa3604a757b6187b45b3e1279e52
Thomas Eng <thomas.eng@shortcut.no>
2019-03-25 14:36:54 +0100

    chore: lint files fix

--- server/manifest.js
+++ server/manifest.js
@@ -9,37 +9,37 @@ Dotenv.config({ path: `${__dirname}/.env` });
 
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
-    server: {
-        host: 'localhost',
-        port: {
-            $env: 'PORT',
-            $coerce: 'number',
-            $default: 3000
-        },
-        debug: {
-            $filter: { $env: 'NODE_ENV' },
-            $default: {
-                log: ['error'],
-                request: ['error']
-            },
-            production: {
-                request: ['implementation']
-            }
-        }
+  server: {
+    host: 'localhost',
+    port: {
+      $env: 'PORT',
+      $coerce: 'number',
+      $default: 3000
     },
-    register: {
-        plugins: [
-            {
-                plugin: '../lib', // Main plugin
-                options: {}
-            },
-            {
-                plugin: {
-                    $filter: { $env: 'NODE_ENV' },
-                    $default: 'hpal-debug',
-                    production: Toys.noop
-                }
-            }
-        ]
+    debug: {
+      $filter: { $env: 'NODE_ENV' },
+      $default: {
+        log: ['error'],
+        request: ['error']
+      },
+      production: {
+        request: ['implementation']
+      }
     }
+  },
+  register: {
+    plugins: [
+      {
+        plugin: '../lib', // Main plugin
+        options: {}
+      },
+      {
+        plugin: {
+          $filter: { $env: 'NODE_ENV' },
+          $default: 'hpal-debug',
+          production: Toys.noop
+        }
+      }
+    ]
+  }
 });
