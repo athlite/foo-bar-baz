950dc0f52cafd83601a9e4777143e9db2ca0872c
Thomas Eng <thomas.eng@shortcut.no>
2019-03-25 15:52:50 +0100

    feat(entry): add conditional

--- entry.sh
+++ entry.sh
@@ -2,9 +2,6 @@
  
 if [ "$NODE_ENV" = "test" ]; then
   yarn test
-elif [ "$NODE_ENV" = "production" ]; then
-  yarn start
 else
-  echo "\$NODE_ENV must be set to test or production."
-  exit 1
+  yarn start
 fi
